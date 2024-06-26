49ee169c928b2ce717e5c6251ec65ac32256b59d
beiyuqu <326936595@qq.com>
2024-06-26 09:46:23 +0800

    Update diy-part1.sh

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -26,7 +26,7 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 pwd
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+./scripts/feeds update -a 
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
